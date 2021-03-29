76178fc92e1ac672ed29dd167bdf5a27c87baa39
Sanaa Syed <sanaa@Sanaas-MacBook-Pro.local>
2021-03-29 16:38:31 -0400

    Adding a button

---
+++ coin-flipper/src/components/FlipButton/FlipButton.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Button } from 'react-bootstrap';
+
+const FlipButton = () => {
+  return (<Button variant="outline-dark">Flip Coin</Button>);
+};
+
+export default FlipButton;
