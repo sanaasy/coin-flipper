03d8b21e9968dc46fd3f75fe94d0ac9de5f95a43
Sanaa Syed <sanaa@Sanaas-MacBook-Pro.local>
2021-04-20 17:30:48 -0400

    Adding styling to the button flip coin

--- coin-flipper/src/components/FlipButton/FlipButton.js
+++ coin-flipper/src/components/FlipButton/FlipButton.js
@@ -1,8 +1,17 @@
 import React from "react";
 import { Button } from 'react-bootstrap';
 
+const location = {
+  position: 'absolute', 
+  left: '45%',
+  bottom: '10%',
+  fontSize: '30px'
+};
+
 const FlipButton = () => {
-  return (<Button variant="outline-dark">Flip Coin</Button>);
+  return (
+    <Button style={location} variant="outline-primary" size="lg">Flip Coin</Button>
+  );
 };
 
 export default FlipButton;
--- coin-flipper/src/index.js
+++ coin-flipper/src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import { AppProvider } from '@shopify/polaris';
 import englishTranslation from '@shopify/polaris/locales/en.json';
 import '@shopify/polaris/dist/styles.css';
